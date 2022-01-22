7ce0356bf90954eab27a56c9e10e4dd72a61f199
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-01-22 12:40:38 +0330

    final configurations

---
+++ public/raw/components/about-me.php
@@ -0,0 +1,20 @@
+<?php
+echo "
+<section class='right-panel '>
+    <article>
+        hello
+    </article>
+    <article>
+        hello
+    </article>
+    <article>
+        hello
+    </article>
+    <article>
+        hello
+    </article>
+</section>
+
+
+
+";
---
+++ public/raw/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="../css/app.css" rel="stylesheet">
+</head>
+<body>
+<?php
+    require "./components/about-me.php";
+?>
+</body>
+</html>
