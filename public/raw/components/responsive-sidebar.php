d0290eb2391db43f36ffe1213cdf1930c0d3095f
Sajad <naghizadehmdsd@gmail.com>
2022-02-13 09:17:24 +0330

    added padding to layer

--- public/raw/components/responsive-sidebar.php
+++ public/raw/components/responsive-sidebar.php
@@ -1,4 +1,4 @@
-<div class="responsive-sidebar shadow drawer drawer-end drawer-mobile h-screen">
+<div class="responsive-sidebar shadow drawer drawer-end drawer-mobile h-full">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle">
     <div class="flex flex-col items-center justify-center drawer-content">
         <label for="my-drawer-2" class="mb-4 btn btn-primary drawer-button lg:hidden text-primary">open menu</label>
