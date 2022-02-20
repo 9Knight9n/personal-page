270021eba855ea2e09f2ddfda3c3e8b8bc386cf3
Sajad <naghizadehmdsd@gmail.com>
2022-02-20 12:05:36 +0330

    home page

--- public/raw/components/responsive-sidebar.php
+++ public/raw/components/responsive-sidebar.php
@@ -1,7 +1,6 @@
 <div class="responsive-sidebar shadow drawer drawer-end drawer-mobile h-full">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle">
     <div class="flex flex-col items-center justify-center drawer-content">
-<!--        <label for="my-drawer-2" class="mb-4 btn btn-primary drawer-button lg:hidden text-primary">open menu</label>-->
         <?php
         require "content-container.php"
         ?>
