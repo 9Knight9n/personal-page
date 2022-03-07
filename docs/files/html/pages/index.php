cc2a0cc56e4acf88f0fc30f054839a41eef27dc9
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-03-07 13:00:05 +0330

    gh pages

--- docs/files/html/pages/index.php
+++ docs/files/html/pages/index.php
@@ -3,24 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <script>
+        let addr = window.location.href
+        if (addr.includes("index.php?page=demo"))
+            window.location.href = addr.replace("index.php?page=demo","demo.html")
+        else if (addr.includes("index.php?page=blog"))
+            window.location.href = addr.replace("index.php?page=blog","blog.html")
+        else if (addr.includes("index.php?page=history"))
+            window.location.href = addr.replace("index.php?page=history","history.html")
+        else if (addr.includes("index.php?page=shop"))
+            window.location.href = addr.replace("index.php?page=shop","shop.html")
+        else if (addr.includes("index.php?page=contact"))
+            window.location.href = addr.replace("index.php?page=contact","contact.html")
+        else if (addr.includes("index.php?page=home"))
+            window.location.href = addr.replace("index.php?page=home","home.html")
+    </script>
 </head>
 <body>
 hello
 </body>
-<script>
-    let addr = window.location.href
-    if (addr.includes("index.php?page=demo"))
-        window.location.href = addr.replace("index.php?page=demo","demo.html")
-    else if (addr.includes("index.php?page=blog"))
-        window.location.href = addr.replace("index.php?page=blog","blog.html")
-    else if (addr.includes("index.php?page=history"))
-        window.location.href = addr.replace("index.php?page=history","history.html")
-    else if (addr.includes("index.php?page=shop"))
-        window.location.href = addr.replace("index.php?page=shop","shop.html")
-    else if (addr.includes("index.php?page=contact"))
-        window.location.href = addr.replace("index.php?page=contact","contact.html")
-    else if (addr.includes("index.php?page=home"))
-        window.location.href = addr.replace("index.php?page=home","home.html")
-</script>
 </html>
 
